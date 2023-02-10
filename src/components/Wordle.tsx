cf658a94aaa9dbed1d347adf143b1ec03f01abc0
Didier Osindero <didierosindero@gmail.com>
2023-02-10 19:28:14 +0000

    Add keyup and submit guess functionality

--- src/components/Wordle.tsx
+++ src/components/Wordle.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useWordle } from "../hooks/useWordle";
 import { GameProgression } from "../utils/types";
 import { GameGrid } from "./GameGrid";
@@ -7,8 +7,19 @@ export const Wordle = (): JSX.Element => {
   //-----------------------------------------------------STATES
   // Set initial game state as "introduction"
 
-  const { currentGuess, previousGuessesEvaluated, turn, gameProgression } =
-    useWordle();
+  const {
+    currentGuess,
+    previousGuessesEvaluated,
+    turn,
+    gameProgression,
+    handleKeyUp,
+  } = useWordle("apple");
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+
+    return () => window.removeEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp]);
 
   return (
     <div className="ctn-main-content">
@@ -18,6 +29,7 @@ export const Wordle = (): JSX.Element => {
         previousGuessesEvaluated={previousGuessesEvaluated}
         turn={turn}
       />
+      <p>Current Guess: {currentGuess}</p>
     </div>
   );
 };
--- src/hooks/useWordle.tsx
+++ src/hooks/useWordle.tsx
@@ -1,15 +1,74 @@
 import { useState } from "react";
+import { evaluateGuess } from "../utils/evaluateGuess";
 import { GameProgression, MarkedGuess } from "../utils/types";
 
-export const useWordle = () => {
+export const useWordle = (targetWord: string) => {
   const [turn, setTurn] = useState<number>(0);
   const [gameProgression, setGameProgression] =
     useState<GameProgression>("introduction");
   const [currentGuess, setCurrentGuess] = useState<string>("");
   const [previousGuessesEvaluated, setPreviousGuessesEvaluated] = useState<
     MarkedGuess[]
-  >([...Array(6)]);
+  >([]);
   const [previousGuesses, setPreviousGuesses] = useState<string[]>([]);
   const [isCorrect, setIsCorrect] = useState<boolean>(false);
-  return { currentGuess, previousGuessesEvaluated, turn, gameProgression };
+
+  //Evaluate Guess
+  //evaluateGuess();
+
+  //Save Guess To previousGuesses and previousEvaluatedGuesses
+  //find out if it is correct and update isCorrect
+  //increase turn by 1
+  const addNewGuess = (currentGuessEvaluated: MarkedGuess) => {
+    if (currentGuess === targetWord) {
+      setIsCorrect(true);
+    }
+    setPreviousGuesses((prev) => [...prev, currentGuess]);
+    setPreviousGuessesEvaluated((prev) => [...prev, currentGuessEvaluated]);
+    setTurn((prev) => prev + 1);
+  };
+
+  //Handle keyup event
+  const handleKeyUp = ({ key }: KeyboardEvent) => {
+    const alphabet = /^[a-z]$/i;
+    const isLessThanSixChars = currentGuess.length < 5;
+    const isFiveChars = currentGuess.length === 5;
+    const isInPreviousGuesses = previousGuesses.includes(currentGuess);
+
+    if (alphabet.test(key) && isLessThanSixChars) {
+      setCurrentGuess((prev) => prev + key);
+    }
+
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => prev.slice(0, -1));
+    }
+
+    if (key === "Enter") {
+      if (isInPreviousGuesses) {
+        alert("You aready guessed this word");
+        return;
+      }
+
+      if (!isFiveChars) {
+        alert("Your word must be five characters longs");
+        return;
+      }
+      const currentGuessEvaluated = evaluateGuess(currentGuess, targetWord);
+      addNewGuess(currentGuessEvaluated);
+      setCurrentGuess("");
+    }
+  };
+
+  console.log("turn:", turn);
+  console.log("is correct:", isCorrect);
+  console.log("previous guesses:", previousGuesses);
+  console.log("previous evaluatedguesses:", previousGuessesEvaluated);
+
+  return {
+    currentGuess,
+    previousGuessesEvaluated,
+    turn,
+    gameProgression,
+    handleKeyUp,
+  };
 };
