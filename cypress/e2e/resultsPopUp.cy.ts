b8ab00496c1d990e6545420c08b9f66c298d69d5
Didier Osindero <didierosindero@gmail.com>
2023-02-12 14:13:27 +0000

    Cypress: Add test to fail game

--- cypress/e2e/resultsPopUp.cy.ts
+++ cypress/e2e/resultsPopUp.cy.ts
@@ -1,5 +1,15 @@
 describe("results pop-up", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+  it("loses the game", () => {
+    cy.intercept("http://localhost:5173").as("getPage");
+    cy.visit("http://localhost:5173");
+    cy.wait("@getPage");
+    cy.get("body")
+      .click()
+      .type("aaaab{enter}")
+      .type("aaaac{enter}")
+      .type("aaaad{enter}")
+      .type("aaaae{enter}")
+      .type("aaaaf{enter}")
+      .type("aaaag{enter}");
   });
 });
